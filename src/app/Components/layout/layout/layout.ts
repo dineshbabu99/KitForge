4fbc7021f903dedd7f7f03ef784d62a256cce41b
dineshbabu99 <dineshkaru28@gmail.com>
2025-08-04 22:55:29 +0530

    Drawer added

--- src/app/Components/layout/layout/layout.ts
+++ src/app/Components/layout/layout/layout.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
